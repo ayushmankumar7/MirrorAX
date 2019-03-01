c84eb55cce1279270b9829ef5565801b5d66f55b
Ayushman <ayushmankumar7@gmail.com>
2019-03-01 18:34:19 +0530

    Uploaded

---
+++ build.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np 
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret,frame = cap.read()
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(25)== 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
