b92b33e8e7935ab270288396e31877ff4c220439
Ayushman <ayushmankumar7@gmail.com>
2019-03-01 23:28:36 +0530

    done

--- build.py
+++ build.py
@@ -1,16 +1,27 @@
+import PIL
+from PIL import Image,ImageTk
+import pytesseract
 import cv2
-import numpy as np 
-
+from tkinter import *
+width, height = 800, 600
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-while True:
-    ret,frame = cap.read()
+root = Tk()
+root.bind('<Escape>', lambda e: root.quit())
+lmain = Label(root)
+lmain.pack()
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(25)== 27:
-        break
+def show_frame():
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = PIL.Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(10, show_frame)
 
-cap.release()
-cv2.destroyAllWindows()
-
-
+show_frame()
+root.mainloop()
